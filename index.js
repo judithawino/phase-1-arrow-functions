af87b3a38e24c04eb0b9a7a0bae01281bf2384a6
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-06-08 09:09:35 +0300

    finished arrow functions lab

---
+++ index.js
@@ -0,0 +1,12 @@
+function  divide(a=2000,b=100){
+    
+    let division = a/b;
+    return division
+}
+divide()
+
+const square = (a)=>a*a;
+square()
+
+const add= (parameter1,parameter2) =>parameter1+parameter2;
+add()
